eff583428b7b51a81f814942534ef087e3d05d33
Jakob <jakob@physik.tu-berlin.de>
2025-02-27 20:05:55 +0100

    Switched off Firebase. Receiving messages while app is inactive works upon resuming.

--- test_notification_app/lib/main.dart
+++ test_notification_app/lib/main.dart
@@ -1,54 +1,55 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart';
+// import 'firebase_options.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'mqtt_service.dart';
 import 'constants.dart';
 
-Set<String> _receivedMessages = {}; // Define globally
+// Set<String> _receivedFirebaseMessages = {}; // Define globally
+Set<String> _receivedMQTTMessages = {}; // Define globally
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+  // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   runApp(const MyApp());
 }
 
 // This handles messages when the app is in the background or terminated
-@pragma('vm:entry-point')
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  final prefs = await SharedPreferences.getInstance();
-  await prefs.reload(); // Force reload before reading
+// @pragma('vm:entry-point')
+// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   final prefs = await SharedPreferences.getInstance();
+//   await prefs.reload(); // Force reload before reading
 
-  print("Received a Firebase message onBackgroundMessage");
+//   print("Received a Firebase message onBackgroundMessage");
 
-  if (message.data.isNotEmpty && message.notification == null) {
-    print("Received a Firebase message with data.");
-    String messageId = message.data['msg_id'] ?? "No message ID";
-    String title = message.data['title'] ?? "No message title";
-    String body = message.data['body'] ?? "No message body";
-    String fullMessage = "msg_id: $messageId, title: $title, body: $body";
-    print("messageId of latest message is $messageId");
+//   if (message.data.isNotEmpty && message.notification == null) {
+//     print("Received a Firebase message with data.");
+//     String messageId = message.data['msg_id'] ?? "No message ID";
+//     String title = message.data['title'] ?? "No message title";
+//     String body = message.data['body'] ?? "No message body";
+//     String fullMessage = "msg_id: $messageId, title: $title, body: $body";
+//     print("messageId of latest message is $messageId");
 
-    _receivedMessages = (prefs.getStringList('receivedMessages') ?? []).toSet();
-    _receivedMessages.add(fullMessage);
-    bool success1 = await prefs.setString('latestMessage', fullMessage);
-    bool success2 = await prefs.setStringList('receivedMessages', _receivedMessages.toList());
-    print("✅ SharedPreferences updated: $success1 and $success2");
-    print("Latest message updated: $fullMessage");
+//     _receivedFirebaseMessages = (prefs.getStringList('receivedFirebaseMessages') ?? []).toSet();
+//     _receivedFirebaseMessages.add(fullMessage);
+//     bool success1 = await prefs.setString('latestMessage', fullMessage);
+//     bool success2 = await prefs.setStringList('receivedFirebaseMessages', _receivedFirebaseMessages.toList());
+//     print("✅ SharedPreferences updated: $success1 and $success2");
+//     print("Latest message updated: $fullMessage");
 
-  } else {
-    print("Received a Firebase notification message without data.");
-  }
-}
+//   } else {
+//     print("Received a Firebase notification message without data.");
+//   }
+// }
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -59,94 +60,116 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   String _mqttMessage = "No MQTT messages yet";
-  String _firebaseMessage = "No Firebase messages yet";
+  // String _firebaseMessage = "No Firebase messages yet";
   late MQTTService _mqttService;
-  late FirebaseService _firebaseService;
+  // late FirebaseService _firebaseService;
 
   @override
   void initState() {
     super.initState();
     _initializeServices();
-    WidgetsBinding.instance.addObserver(this);
-    _loadLatestMessage();
-    _loadReceivedMessages();
+    // WidgetsBinding.instance.addObserver(this);
+    // _loadLatestMessage();
+    // _loadReceivedFirebaseMessages();
   }
 
   @override
   void dispose() {
     _mqttService.disconnect();
-    WidgetsBinding.instance.removeObserver(this);
+    // WidgetsBinding.instance.removeObserver(this);
     super.dispose();
   }
 
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) async {
-    print("App state changed: $state");
-    if (state == AppLifecycleState.resumed) {
-      print("App state resumed, loading latest message...");
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      await prefs.reload();  // Force reload
+  // @override
+  // void didChangeAppLifecycleState(AppLifecycleState state) async {
+  //   print("App state changed: $state");
+  //   if (state == AppLifecycleState.resumed) {
+  //     print("App state resumed, loading latest message...");
+  //     SharedPreferences prefs = await SharedPreferences.getInstance();
+  //     await prefs.reload();  // Force reload
 
-      String? latestMessage = prefs.getString('latestMessage');
-      print("Load latest message: $latestMessage");
-      if (latestMessage != null) {
-        setState(() {
-          _firebaseMessage = latestMessage;
-        });
-      } else {
-        print("No latest message found");
-      }
+  //     String? latestMessage = prefs.getString('latestMessage');
+  //     print("Load latest message: $latestMessage");
+  //     if (latestMessage != null) {
+  //       setState(() {
+  //         _firebaseMessage = latestMessage;
+  //       });
+  //     } else {
+  //       print("No latest message found");
+  //     }
 
-      List<String>? messages = prefs.getStringList('receivedMessages');
-      if (messages != null) {
-        setState(() {
-          _receivedMessages = messages.toSet()..toList().sort();
-        });
-      }
-    }
-  }
+  //     List<String>? messages = prefs.getStringList('receivedFirebaseMessages');
+  //     if (messages != null) {
+  //       setState(() {
+  //         _receivedFirebaseMessages = messages.toSet()..toList().sort();
+  //       });
+  //     }
+  //   }
+  // }
 
   void _initializeServices() {
     _mqttService = MQTTService(onMessageReceived: _onMqttMessageReceived);
     _mqttService.initializeMQTT();
-
-    _firebaseService = FirebaseService(onMessageReceived: _onFirebaseMessageReceived);
-    _firebaseService.initializeFirebase();
+    _loadReceivedMQTTMessages();
+    // _firebaseService = FirebaseService(onMessageReceived: _onFirebaseMessageReceived);
+    // _firebaseService.initializeFirebase();
   }
 
-  void _onMqttMessageReceived(String message) {
+  void _onMqttMessageReceived(String message) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.reload();  // Force reload
+    _receivedMQTTMessages = (prefs.getStringList('receivedMQTTMessages') ?? []).toSet();
+    _receivedMQTTMessages.add(message);
+    
+    bool success2 = await prefs.setStringList('receivedMQTTMessages', _receivedMQTTMessages.toList());
+
     setState(() {
       _mqttMessage = message;
     });
   }
 
-  void _onFirebaseMessageReceived(String message) {
-    setState(() {
-      _firebaseMessage = message;
-    });
-  }
+  // void _onFirebaseMessageReceived(String message) {
+  //   setState(() {
+  //     _firebaseMessage = message;
+  //   });
+  // }
 
-  Future<void> _loadLatestMessage() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.reload(); // Force reload before reading
+  // Future<void> _loadLatestMessage() async {
+  //   SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   await prefs.reload(); // Force reload before reading
 
-    String? latestMessage = prefs.getString('latestMessage');
-    print("Load latest message: $latestMessage");
-    if (latestMessage != null) {
-      setState(() {
-        _firebaseMessage = latestMessage;
-      });
-    }
-  }
+  //   String? latestMessage = prefs.getString('latestMessage');
+  //   print("Load latest message: $latestMessage");
+  //   if (latestMessage != null) {
+  //     setState(() {
+  //       _firebaseMessage = latestMessage;
+  //     });
+  //   }
+  // }
 
-  Future<void> _loadReceivedMessages() async {
+  // Future<void> _loadReceivedFirebaseMessages() async {
+  //   SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   await prefs.reload(); // Force reload before reading
+    
+  //   List<String>? messages = prefs.getStringList('receivedFirebaseMessages');
+  //   if (messages != null) {
+  //     setState(() {
+  //       _receivedFirebaseMessages = messages.toSet()..toList().sort();
+  //     });
+  //   }
+  // }
+
+  Future<void> _loadReceivedMQTTMessages() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.reload(); // Force reload before reading
     
-    List<String>? messages = prefs.getStringList('receivedMessages');
+    List<String>? messages = prefs.getStringList('receivedMQTTMessages');
     if (messages != null) {
       setState(() {
-        _receivedMessages = messages.toSet()..toList().sort();
+        _receivedMQTTMessages = messages.toSet()..toList().sort();
+        if (messages.isNotEmpty) {
+          _mqttMessage = messages.last;
+        }
       });
     }
   }
@@ -183,12 +206,12 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text("Latest Firebase Message:"),
-              Text(
-                _firebaseMessage,
-                style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 20),
+              // const Text("Latest Firebase Message:"),
+              // Text(
+              //   _firebaseMessage,
+              //   style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              // ),
+              // const SizedBox(height: 20),
               const Text("Latest MQTT Message:"),
               Text(
                 _mqttMessage,
@@ -201,7 +224,7 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
               ),
               const SizedBox(height: 20),
               const Text("Recent Messages:"),
-              ..._receivedMessages.toList().reversed.take(10).map((message) => Text(
+              ..._receivedMQTTMessages.toList().reversed.take(10).map((message) => Text(
                 message,
                 style: const TextStyle(fontSize: 14),
               )).toList(),
@@ -213,64 +236,64 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   }
 }
 
-class FirebaseService {
-  static final FirebaseMessaging _messaging = FirebaseMessaging.instance;
-  final Function(String) onMessageReceived;
+// class FirebaseService {
+//   static final FirebaseMessaging _messaging = FirebaseMessaging.instance;
+//   final Function(String) onMessageReceived;
 
-  FirebaseService({required this.onMessageReceived});
+//   FirebaseService({required this.onMessageReceived});
 
-  Future<void> initializeFirebase() async {
-    NotificationSettings settings = await _messaging.requestPermission();
-    print("🔐 Permission status: ${settings.authorizationStatus}");
+//   Future<void> initializeFirebase() async {
+//     NotificationSettings settings = await _messaging.requestPermission();
+//     print("🔐 Permission status: ${settings.authorizationStatus}");
 
-    String? apnsToken = await FirebaseMessaging.instance.getAPNSToken();
-    print("APNs Token: $apnsToken");
+//     String? apnsToken = await FirebaseMessaging.instance.getAPNSToken();
+//     print("APNs Token: $apnsToken");
 
-    String? token = await _messaging.getToken();
-    print("📲 Initial FCM Token: $token");
-    if (token != null) sendTokenToBackend(token);
+//     String? token = await _messaging.getToken();
+//     print("📲 Initial FCM Token: $token");
+//     if (token != null) sendTokenToBackend(token);
 
-    _messaging.onTokenRefresh.listen(sendTokenToBackend);
+//     _messaging.onTokenRefresh.listen(sendTokenToBackend);
 
-    FirebaseMessaging.onMessage.listen(handleMessage);
-    FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
-  }
+//     FirebaseMessaging.onMessage.listen(handleMessage);
+//     FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+//   }
 
-  static void sendTokenToBackend(String token) async {
-    var backendURL = "$BASE_URL/register-token/";
-    var response = await http.post(
-      Uri.parse(backendURL),
-      headers: {"Content-Type": "application/json"},
-      body: '{"token": "$token"}',
-    );
-    print("✅ Token Sent to Backend: ${response.body}");
-  }
+//   static void sendTokenToBackend(String token) async {
+//     var backendURL = "$BASE_URL/register-token/";
+//     var response = await http.post(
+//       Uri.parse(backendURL),
+//       headers: {"Content-Type": "application/json"},
+//       body: '{"token": "$token"}',
+//     );
+//     print("✅ Token Sent to Backend: ${response.body}");
+//   }
 
-  void handleMessage(RemoteMessage message) async {
-    if (message.data.isNotEmpty && message.notification == null) {
-      print("Received a Firebase message with data.");
+//   void handleMessage(RemoteMessage message) async {
+//     if (message.data.isNotEmpty && message.notification == null) {
+//       print("Received a Firebase message with data.");
       
-      String messageId = message.data['msg_id'] ?? "0";
-      String title = message.data['title'] ?? "No message title";
-      String body = message.data['body'] ?? "No message body";
-      String fullMessage = "msg_id: $messageId, title: $title, body: $body";
+//       String messageId = message.data['msg_id'] ?? "0";
+//       String title = message.data['title'] ?? "No message title";
+//       String body = message.data['body'] ?? "No message body";
+//       String fullMessage = "msg_id: $messageId, title: $title, body: $body";
       
-      onMessageReceived(fullMessage);
+//       onMessageReceived(fullMessage);
 
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-      await prefs.reload(); // Force reload before reading
+//       SharedPreferences prefs = await SharedPreferences.getInstance();
+//       await prefs.reload(); // Force reload before reading
       
-      _receivedMessages = (prefs.getStringList('receivedMessages') ?? []).toSet();
-      _receivedMessages.add(fullMessage);
+//       _receivedFirebaseMessages = (prefs.getStringList('receivedFirebaseMessages') ?? []).toSet();
+//       _receivedFirebaseMessages.add(fullMessage);
       
-      bool success1 = await prefs.setString('latestMessage', fullMessage);
-      bool success2 = await prefs.setStringList('receivedMessages', _receivedMessages.toList());
+//       bool success1 = await prefs.setString('latestMessage', fullMessage);
+//       bool success2 = await prefs.setStringList('receivedFirebaseMessages', _receivedFirebaseMessages.toList());
       
-      print("✅ SharedPreferences updated: $success1 and $success2");
-      print("Latest message updated: $fullMessage");
+//       print("✅ SharedPreferences updated: $success1 and $success2");
+//       print("Latest message updated: $fullMessage");
 
-    } else {
-      print("Received a Firebase notification message without data.");
-    }
-  }
-}
+//     } else {
+//       print("Received a Firebase notification message without data.");
+//     }
+//   }
+// }
--- test_notification_app/lib/mqtt_service.dart
+++ test_notification_app/lib/mqtt_service.dart
@@ -1,5 +1,6 @@
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'constants.dart';
@@ -11,7 +12,12 @@ class MQTTService {
   MQTTService({required this.onMessageReceived});
 
   Future<void> initializeMQTT() async {
-    final clientId = 'flutter_client_${DateTime.now().millisecondsSinceEpoch}';
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? clientId = prefs.getString('mqtt_client_id');
+    if (clientId == null) {
+      clientId = 'flutter_client_${DateTime.now().millisecondsSinceEpoch}';
+      await prefs.setString('mqtt_client_id', clientId);
+    }
     client = MqttServerClient(MQTT_BROKER, clientId);
     client.port = 1883;
     client.keepAlivePeriod = 60;
@@ -19,10 +25,11 @@ class MQTTService {
     client.onConnected = () => debugPrint('Connected to MQTT broker');
     client.onDisconnected = () => debugPrint('Disconnected from MQTT broker');
     client.autoReconnect = true;  // Automatically reconnect on disconnection
+    // client.setProtocolV311();  // Ensures MQTT 3.1.1 support
 
     final connMessage = MqttConnectMessage()
         .withClientIdentifier(clientId)
-        .startClean()
+        // .startClean()
         .withWillQos(MqttQos.atLeastOnce);
     client.connectionMessage = connMessage;
 
@@ -35,9 +42,8 @@ class MQTTService {
 
     client.subscribe(MQTT_TOPIC, MqttQos.atLeastOnce);
 
-    client.updates!.listen((List<MqttReceivedMessage<MqttMessage?>>? c) {
+    client.updates!.listen((List<MqttReceivedMessage<MqttMessage?>>? c) async {
       final recMessage = c![0].payload as MqttPublishMessage;
-
       final payloadBytes = recMessage.payload.message;
 
       debugPrint('Received MQTT message: $payloadBytes');
@@ -48,7 +54,14 @@ class MQTTService {
         String msg_id = payload['msg_id'].toString();
         String title = payload['title'] ?? "No message body";
         String body = payload['body'] ?? "No message body";
-        onMessageReceived("msg_id: $msg_id, title: $title, body: $body");
+        String message = "msg_id: $msg_id, title: $title, body: $body";
+
+        final SharedPreferences prefs = await SharedPreferences.getInstance();
+        List<String> messages = prefs.getStringList('receivedMQTTMessages') ?? [];
+        messages.add(message);
+        await prefs.setStringList('receivedMQTTMessages', messages);
+
+        onMessageReceived(message);
       } catch (e) {
         debugPrint('Error decoding payload: $e');
       }
