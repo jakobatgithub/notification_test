cbe63c1aeafbb85c1696f77f1d9dc1461657c1c8
Jakob <jakob@physik.tu-berlin.de>
2025-03-14 09:24:54 +0100

    Turned 'mqtt_token' into a ViewSet.

--- backend/notifications/views.py
+++ backend/notifications/views.py
@@ -2,7 +2,7 @@ import json
 
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from rest_framework.decorators import action, api_view, authentication_classes, permission_classes
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 
 from rest_framework_simplejwt.authentication import JWTAuthentication
