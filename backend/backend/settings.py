881697a789e128f943135eba2304d4bd8ac6c4a1
Jakob <jakob@physik.tu-berlin.de>
2025-03-28 08:14:44 +0100

    Added cert to settings.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -175,6 +175,7 @@ EMQX_PORT = 8883
 EMQX_WEBHOOK_SECRET = os.environ.get("EMQX_WEBHOOK_SECRET")
 EMQX_MAX_RETRIES = 10  # Maximum retry attempts
 EMQX_RETRY_DELAY = 3   # Wait time in seconds before retrying
+EMQX_TLS_CA_CERTS = '/app/certs/emqx.crt'
 
 # SIMPLE_JWT settings
 SIMPLE_JWT = {
