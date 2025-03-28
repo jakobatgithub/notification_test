8c99ea943239dca5e42b4b8ef50b5d0acb9c5037
Jakob <jakob@physik.tu-berlin.de>
2025-03-28 14:26:20 +0100

    Broadcast MQTT data messages upon client connect and disconnect events.

--- backend/notifications/receivers.py
+++ backend/notifications/receivers.py
@@ -1,21 +1,45 @@
 ## notifications/receivers.py
 
+import json
+
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 
 from django_emqx.signals import emqx_device_connected, new_emqx_device_connected, emqx_device_disconnected
+from django_emqx.utils import send_mqtt_message
+from django_emqx.models import Message
+
 
 @receiver(emqx_device_connected)
 def handle_emqx_device_connected(sender, user_id, device_id, ip_address, **kwargs):
     user = get_user_model().objects.filter(id=int(user_id)).first()
     print(f"User {user} connected on device {device_id} (IP: {ip_address})")
+    recipients = get_user_model().objects.all()
+    data = json.dumps({
+        "device_id": device_id,
+        "user": user,
+        "event": "device_connected",
+    })
+    message = Message.objects.create(data=data)
+    for recipient in recipients:
+        send_mqtt_message(recipient, message)
 
 @receiver(new_emqx_device_connected)
 def handle_new_emqx_device_connected(sender, user_id, device_id, ip_address, **kwargs):
     user = get_user_model().objects.filter(id=int(user_id)).first()
     print(f"User {user} connected on new device {device_id} (IP: {ip_address})")
 
+
 @receiver(emqx_device_disconnected)
 def handle_emqx_device_disconnected(sender, user_id, device_id, ip_address, **kwargs):
     user = get_user_model().objects.filter(id=int(user_id)).first()
     print(f"User {user} disconnected from device {device_id}")
+    recipients = get_user_model().objects.all()
+    data = json.dumps({
+        "device_id": device_id,
+        "user": user,
+        "event": "device_disconnected",
+    })
+    message = Message.objects.create(data=data)
+    for recipient in recipients:
+        send_mqtt_message(recipient, message)
