7cb381f2360f8a79c2323c5cffd9ab7af6e2a409
Jakob <jakob@physik.tu-berlin.de>
2025-03-30 15:25:15 +0200

    Renamed some methods.

--- test_notification_app/lib/providers/device_provider.dart
+++ test_notification_app/lib/providers/device_provider.dart
@@ -13,6 +13,16 @@ class DeviceProvider with ChangeNotifier {
     notifyListeners();
   }
 
+  void addNewDevice({
+    required int user,
+    required String clientID,
+    bool active = true,
+  }) {
+    final newDevice = Device(user: user, clientID: clientID, active: active);
+    _devices.add(newDevice);
+    notifyListeners();
+  }
+
   void updateDeviceFields({required String clientID, bool? active}) {
     final device = getDeviceByClientId(clientID);
     if (device != null) {
@@ -35,16 +45,5 @@ class DeviceProvider with ChangeNotifier {
       }
     }
     return null;
-  }
-
-  void createDevice({
-    required int user,
-    required String clientID,
-    bool active = true,
-  }) {
-    final newDevice = Device(user: user, clientID: clientID, active: active);
-    _devices.add(newDevice);
-    debugPrint("New Device created");
-    notifyListeners();
   }
 }
