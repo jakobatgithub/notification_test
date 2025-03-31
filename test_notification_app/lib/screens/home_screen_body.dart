5ed97ab2ba0cdca33f7953bd3b84ae8a48ef143a
Jakob <jakob@physik.tu-berlin.de>
2025-03-31 11:43:50 +0200

    Messages and Devices are shown in frontend.

--- test_notification_app/lib/screens/home_screen_body.dart
+++ test_notification_app/lib/screens/home_screen_body.dart
@@ -5,23 +5,32 @@ import 'package:flutter/material.dart';
 import '/widgets/latest_message_widget.dart';
 import '/widgets/send_notification_button.dart';
 import '/widgets/recent_messages_widget.dart';
+import '/widgets/device_list_widget.dart';
 
 class HomeScreenBody extends StatelessWidget {
   const HomeScreenBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          LatestMessageWidget(),
-          const SizedBox(height: 20),
-          SendNotificationButton(),
-          const SizedBox(height: 20),
-          const Text("Recent Messages:"),
-          RecentMessagesWidget(),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            LatestMessageWidget(),
+            const SizedBox(height: 20),
+            SendNotificationButton(),
+            const SizedBox(height: 20),
+            const Text("Recent Messages:"),
+            RecentMessagesWidget(),
+            const SizedBox(height: 20),
+            const Text("Devices:"),
+            SizedBox(
+              height: 300, // Adjust height as needed
+              child: DeviceListWidget(),
+            ),
+          ],
+        ),
       ),
     );
   }
