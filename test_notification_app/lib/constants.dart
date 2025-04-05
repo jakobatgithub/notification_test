e1e2a4ab39ecc48c5f6e00cab1ef76f920cfcc09
Jakob <jakob@physik.tu-berlin.de>
2025-04-05 12:30:41 +0200

    Switch off TLS. iOS and Android emulators use different URLs for localhost.

--- test_notification_app/lib/constants.dart
+++ test_notification_app/lib/constants.dart
@@ -1,7 +1,11 @@
-const String baseURL = "https://192.168.178.33";
-const String mqttBroker = "192.168.178.33";
-const int mqttPort = 8883;
-const bool enableTLS = true;
+import 'dart:io';
 
-// const int mqttPort = 1883;
-// const bool enableTLS = false;
+final String baseURL = Platform.isIOS ? "http://localhost" : "http://10.0.2.2";
+final String mqttBroker = Platform.isIOS ? "localhost" : "10.0.2.2";
+const int mqttPort = 1883;
+const bool enableTLS = false;
+
+// const String baseURL = "https://192.168.178.33";
+// const String mqttBroker = "192.168.178.33";
+// const int mqttPort = 8883;
+// const bool enableTLS = true;
